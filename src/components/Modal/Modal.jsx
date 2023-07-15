081b584626cc9d0d5d4b691ecb0e4c7909234f01
Dairon Danilo Dorado Castillo <ddanilodorado@gmail.com>
2023-07-15 07:59:33 -0500

    changed modal styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,13 @@
 import classes from "./Modal.module.css";
-
+import { CgClose } from "react-icons/cg";
+import { AiFillCloseSquare } from "react-icons/ai";
 const Modal = ({ children, onClose }) => {
   return (
     <div className={classes.container}>
-      <div className={classes.backdrop} onClick={onClose} />
+      <div className={classes.backdrop} onClick={null} />
       <dialog open className={classes.modal} onClick={null}>
+        <CgClose className={classes.icon} onClick={onClose} />
+
         {children}
       </dialog>
     </div>
